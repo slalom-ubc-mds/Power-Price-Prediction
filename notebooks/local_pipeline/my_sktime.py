962e3451a8909ebee9182c199944ed1b791db3e3
Arjun Radhakrishnan <rkrishnan.arjun@gmail.com>
2023-06-13 00:08:28 -0700

    Corrected my_sktime

--- notebooks/local_pipeline/my_sktime.py
+++ notebooks/local_pipeline/my_sktime.py
@@ -9,9 +9,11 @@ __all__ = ["_BaseWindowForecaster"]
 import numpy as np
 import pandas as pd
 
-from sktime.forecasting.base._base import DEFAULT_ALPHA, BaseForecaster
+# from sktime.forecasting.base._base import DEFAULT_ALPHA, BaseForecaster
 from sktime.forecasting.model_selection import CutoffSplitter
 from sktime.utils.datetime import _shift
+
+from my_base import DEFAULT_ALPHA, BaseForecaster
 
 
 class _BaseWindowForecaster(BaseForecaster):
