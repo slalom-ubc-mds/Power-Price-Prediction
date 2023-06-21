c1c8e66eb95d53062a7294fb9ea766a4be8ae122
Mehdi Naji <snaji001@ucr.edu>
2023-06-21 13:39:25 -0700

    update usage

--- notebooks/eda/eda.py
+++ notebooks/eda/eda.py
@@ -1,7 +1,7 @@
 #Explanatory Data Analysis
 """
 Usage:
-  eda.py load_data <arg> 
+  eda.py load_data <arg>
   eda.py plot_df_timeseries <df> 
   eda.py plot_scatter <df1> <df1_column> <df2> <df2_column>
   eda.py correlation <df> 
@@ -187,12 +187,7 @@ def plot_daily_seasonality(df, df_column):
 
     # Create subplots with a grid layout of 2 rows and 4 columns
     fig = make_subplots(rows=4, cols=2, subplot_titles=days_of_week)
-<<<<<<< HEAD
 
-=======
-    fig.update_layout(height=1200)
-  
->>>>>>> d2a7fd76938cd71bd2780ff78d06fc767bbb6ced
     # Iterate over each day and add a scatter plot to the corresponding subplot
     for i, day in enumerate(days_of_week):
         subplot_row = (i % 4) + 1
